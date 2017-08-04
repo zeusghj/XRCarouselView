2d405da7426efd564f4d26bd7498aa9ef394c3a1
yd13150@vip.qq.com <admin@admindeMacBook-Pro.local>
2017-08-04 16:20:56 +0800

    修复页面退出时，控件内存不释放的bug

--- XRCarouselView/XRCarouselView.h
+++ XRCarouselView/XRCarouselView.h
@@ -108,8 +108,8 @@ UIImage *gifImageNamed(NSString *imageName);
 
 
 /**
- *  每一页停留时间，默认为5s，最少2s
- *  当设置的值小于2s时，则为默认值
+ *  每一页停留时间，默认为5s，最少1s
+ *  当设置的值小于1s时，则为默认值
  */
 @property (nonatomic, assign) NSTimeInterval time;
 
